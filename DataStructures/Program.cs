a8a8d5117163706d6afa93e015cef87f36af4261
Mayur Mahadev Kalekar <87481060+mayurkalekar087@users.noreply.github.com>
2021-08-12 22:06:31 +0530

    Update Program.cs

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -6,12 +6,10 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             LinkedList List = new LinkedList();
             List.Add(56);
             List.Add(30);
             List.Add(70);
-            //List.Display();
         }
     }
 }
