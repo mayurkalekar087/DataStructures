9c9175f07835bdd8db47d4363a9101549b956822
Mayur Mahadev Kalekar <87481060+mayurkalekar087@users.noreply.github.com>
2021-08-12 22:07:52 +0530

    Update LinkedList.cs

--- DataStructures/LinkedList.cs
+++ DataStructures/LinkedList.cs
@@ -7,40 +7,9 @@ namespace DataStructures
    
     public class LinkedList
     {
-        internal Node head;
-        internal void Add(int data)
-        {
-            Node node = new Node(data);
-            if (this.head == null)
-            {
-                this.head = node;
-            }
-            else
-            {
-                Node temp = head;
-                while (temp.next != null)
-                {
-                    temp = temp.next;
-                }
-                temp.next = node;
-            }
-            Console.WriteLine("{0} inserted into linked", node.data);
-        }
-        internal void Display()
-        {
-            Node temp = this.head;
-            if(temp == null)
-            {
-                Console.WriteLine("Linked is empty");
-                return;
-            }
-            while(temp != null)
-            {
-                Console.WriteLine(temp.data + " ");
-                temp = temp.next;
-            }
-           
-        }
+       
     }
+           
 }
+ 
 
