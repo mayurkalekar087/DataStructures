86bbaea7f47764cbd3063f6045fc3b7bffd2274b
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-08-13 14:01:07 +0530

    feat:Insert LinkedList

--- DataStructures/LinkedList.cs
+++ DataStructures/LinkedList.cs
@@ -39,8 +39,38 @@ namespace DataStructures
                 Console.WriteLine(temp.data + " ");
                 temp = temp.next;
             }
-
-
         }
-    }
+        internal Node InsertAtParticularPosition(int position, int data)
+        {
+            if (position < 1)
+            {
+                Console.WriteLine("Invalid Position");
+            }
+            if (position == 1)
+            {
+                Node newNode = new Node(data);
+                newNode.next = this.head;
+                head = newNode;
+            }
+            else
+            {
+                while (position-- != 0)
+                {
+                    if (position == 1)
+                    {
+                        var node = new Node(data);
+                        node.next = this.head.next;
+                        head.next = node;
+                        break;
+                    }
+                    head = head.next;
+                }
+                if (position != 1)
+                    Console.WriteLine("Position out of range");
+            }
+            Console.WriteLine("Inserted value is : " + head);
+            return head;
+        }
+      
+     }
 }
--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -8,9 +8,11 @@ namespace DataStructures
         {
             LinkedList linkedlist = new LinkedList();
             linkedlist.Add(56);
-            linkedlist.Add(30);
+            //linkedlist.Add(30);
             linkedlist.Add(70);
+            linkedlist.InsertAtParticularPosition(2, 30);
             linkedlist.Display();
+            Console.ReadKey();
         }
     }
 }
