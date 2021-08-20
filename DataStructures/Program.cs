601c97fa68bd2c5e00092dd5b1f4e57a3fb6d0c0
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-08-20 13:29:12 +0530

    feat:Ability to sort linked list in ascending order

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -9,17 +9,26 @@ namespace DataStructures
             LinkedList linkedlist = new LinkedList();
             linkedlist.Add(56);
             linkedlist.Add(30);
+            linkedlist.Add(40);
             linkedlist.Add(70);
             linkedlist.Display();
             Console.WriteLine("--------------");
 
+            sortLinkedList sortlinkedlist = new sortLinkedList();
+            sortlinkedlist.Add(56);
+            sortlinkedlist.Add(30);
+            sortlinkedlist.Add(40);
+            sortlinkedlist.Add(70);
+            sortlinkedlist.Display();
+            Console.WriteLine("--------------");
+
             //linkedlist.RemoveFirstNode();
             //linkedlist.RemoveLastNode();
-            linkedlist.search(30);
-            linkedlist.InsertAtParticularPosition(3, 40);
-            linkedlist.Delete(40);
-            linkedlist.Display();
-            Console.ReadKey();
+            //linkedlist.search(30);
+            //linkedlist.InsertAtParticularPosition(3, 40);
+            //linkedlist.Delete(40);
+            //linkedlist.Display();
+            //Console.ReadKey();
         }
     }
 }
---
+++ DataStructures/sortLinkedList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    class sortLinkedList
+    {
+        internal Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            Node current = head, previous = null;
+            while (current != null && current.data < node.data)
+            {
+                previous = current;
+                current = current.next;
+            }
+            if (previous == null)
+                head = node;
+            else
+            {
+                previous.next = node;
+            }
+            node.next = current;
+
+        }
+        internal void Display()
+        {
+            if (this.head == null)
+                Console.WriteLine("The list is empty.");
+            else
+            {
+                Node temp = head;
+                while (temp != null)
+                {
+                    Console.WriteLine("Element : " + temp.data);
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
+
